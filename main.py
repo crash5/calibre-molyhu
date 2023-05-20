e299254945938ffaf61997707fd28ad2ccef086d
Imre NAGY <crash@veluna.eu>
2023-05-20 13:24:01 +0200

    Update example main for local search

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import argparse
 import urllib.request
 
 import moly_hu.moly_hu as molyhu
 
 
 if __name__ == '__main__':
-    print('start')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('search_for', type=str, default= 'Raymond Feist Magus')
+    parser.add_argument('-c', '--count', type=int, default=1)
+    args = parser.parse_args()
+    search_for = args.search_for
+    max_result = args.count
 
-    dl = lambda x: urllib.request.urlopen(x).read().decode('utf-8')
-    search = lambda x: molyhu.search(x, dl)
+    browser = lambda url: urllib.request.urlopen(url).read().decode('utf-8')
 
-    book_ids = molyhu.book_ids_for('magus', ['raymond feist'], {}, search)
-    book = molyhu.book_for_id(book_ids[0], dl)
-    print(book)
-    print('end')
+    book_ids = molyhu.search(search_for, browser)
+    for count, book_id in enumerate(book_ids, start=1):
+        if count > max_result:
+            break
+        book = molyhu.book_for_id(book_id, browser)
+        print(book)
